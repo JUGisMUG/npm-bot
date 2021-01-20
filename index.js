ec7f21538b39fbac0cf924899dbcdc075411d1e6
JUGisMUGlol <AtGum11@gmail.com>
2021-01-20 09:16:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const discord = require('discord.js');
+const client = new discord.Client();
+const fetch = require('node-fetch');
+
+const prefix = '?';
+const token = 'bruh you really want token? put your own here';
+
+client.on('ready', () => {
+	console.log('Am ready kek');
+	client.user.setActivity('Anime' + '🤤', { type: 'WATCHING' });
+});
+
+client.on('message', async message => {
+	if (message.author.bot || !message.content.startsWith(prefix)) return;
+
+	const args = message.content
+		.slice(prefix.length)
+		.trim()
+		.split(/ +/g);
+	const cmd = args.shift().toLowerCase();
+
+	if (cmd === 'info') {
+		if (!args[0])
+			return message.channel.send(
+				'Please provide the npm package for which you need information.'
+			);
+
+		let msg = await message.channel.send(
+			new discord.MessageEmbed()
+				.setAuthor(
+					`Fetching information for: ${args[0]}`,
+					client.user.displayAvatarURL()
+				)
+				.setColor('GREEN')
+		);
+		
+		try {
+		  fetch(`https://registry.npmjs.org/${args[0]}`)
+			.then(res => res.json())
+			.then(response => {
+			  if (response.error) return msg.edit(new discord.MessageEmbed().setAuthor(`Invalid Package provided: ${args[0]}`, client.user.displayAvatarURL()).setColor('RED'))
+			  
+			  let latest = response['dist-tags'].latest
+			  let repository = response.versions[latest].repository.url || 'None'
+			  let keywords = response.versions[latest].keywords
+			  let maintainers = response.versions[latest].maintainers.map(object => object.name).join(', ')
+			  let typescript = () => {
+			    if (response.versions[latest].types) return 'Yes'
+			    if (response.versions[latest].types === undefined) return 'No'
+			  }
+			  
+			  if (keywords === undefined) {
+			    keywords = 'None'
+			  } else if (keywords !== undefined) keywords = keywords.join(', ')
+			  
+			  if (repository.includes('git+')) repository = repository.replace('git+', '')
+			  if (repository.includes('.git')) repository = repository.replace('.git', '')
+			  
+			  let embed = new discord.MessageEmbed()
+			  .setAuthor(args[0] + ' - ' + latest, 'https://i.gyazo.com/3b2e93eb22aff52694daf56b5cf1ad75.png')
+			  .setURL(`https://www.npmjs.com/package/${args[0]}`)
+			  .setDescription(response.description || 'None')
+			  .setThumbnail(client.user.displayAvatarURL())
+			  .setColor('GREEN')
+			  .setFooter(keywords)
+			  .addFields({
+			    name: 'Maintainers:',
+			    value: maintainers
+			  }, {
+			    name: 'Dependencies:',
+			    value: Object.keys(response.versions[latest].dependencies).join(', ') || 'None'
+			  }, {
+			    name: 'License:',
+			    value: response.versions[latest].license || 'None'
+			  }, {
+			    name: 'Repository:',
+			    value: `[GitHub Repository](${repository})`
+			  }, {
+			    name: 'Typescript Supoort:',
+			    value: typescript()
+			  })
+			  
+			  msg.edit(embed)
+			});
+		} catch (err) {
+	    msg.delete()
+	    message.channel.send(new discord.MessageEmbed().setTitle(`An unexpected error occured:`).setDescription(err))
+		}
+	}
+});
+
+client.login(token);
