11508687454553321f7e4bf94661e64cec58f05c
JUGisMUG <70059840+JUGisMUG@users.noreply.github.com>
2021-02-06 23:26:52 +0500

    Fixed bugs

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const discord = require('discord.js');
 const client = new discord.Client();
+const eventEmitter = require('events');
+
+const error = new eventEmitter;
+
 const fetch = require('node-fetch');
 
 const prefix = '?';
@@ -10,26 +14,60 @@ client.on('ready', () => {
 	client.user.setActivity('Anime' + '🤤', { type: 'WATCHING' });
 });
 
+error.on('error', (msg, err) => {
+  msg.edit(new discord.MessageEmbed().setAuthor(err, client.user.displayAvatarURL()).setColor('RED'))
+});
+
 client.on('message', async message => {
 	if (message.author.bot || !message.content.startsWith(prefix)) return;
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const args = message.content
+		.slice(prefix.length)
+		.trim()
+		.split(/ +/g);
 	const cmd = args.shift().toLowerCase();
 
 	if (cmd === 'info') {
-		if (!args[0]) return message.channel.send('Please provide the npm package for which you need information.');
+		if (!args[0])
+			return message.channel.send(
+				'Please provide the npm package for which you need information.'
+			);
 
-		let msg = await message.channel.send(new discord.MessageEmbed().setAuthor(`Fetching information for: ${args[0]}`, client.user.displayAvatarURL()).setColor('GREEN'));
+		let msg = await message.channel.send(
+			new discord.MessageEmbed()
+				.setAuthor(
+					`Fetching information for: ${args[0]}`,
+					client.user.displayAvatarURL()
+				)
+				.setColor('GREEN')
+		);
 		
 		try {
 		  fetch(`https://registry.npmjs.org/${args[0]}`)
 			.then(res => res.json())
 			.then(response => {
-			  if (response.error) return msg.edit(new discord.MessageEmbed().setAuthor(`Invalid Package provided: ${args[0]}`, client.user.displayAvatarURL()).setColor('RED'))
+			  if (response.error) return error.emit('error', msg, `Unable to fetch information for the package: ${args[0]}`);
 			  
-			  let latest = response['dist-tags'].latest
-			  let repository = response.versions[latest].repository.url || 'None'
-			  let keywords = response.versions[latest].keywords
+			 let latest;
+			 if (!response['dist-tags']) {
+			   return error.emit('error', msg, `Unable to fetch information for the package: ${args[0]}`);
+			 } else if (response['dist-tags']) {
+			   let latest = response['dist-tags'].latest;
+			 }
+			 
+			 let repository;
+			 let keywords;
+			 if (!response.versions[latest]) {
+			   return error.emit('error', msg, `Unable to fetch information for the package: ${args[0]}`);
+			 } else if (response.versions[latest]) {
+			   repository = response.versions[latest].repository.url;
+			   keywords = response.versions[latest].keywords;
+			   keywords = keywords.join(', ');
+			 } else {
+			   repository = 'None';
+			   keywords = 'None';
+			 }
+			  
 			  let maintainers = response.versions[latest].maintainers.map(object => object.name).join(', ')
 			  
 			  let dependencies;
@@ -48,7 +86,7 @@ client.on('message', async message => {
 			  .setDescription(response.description ? response.description : 'None')
 			  .setThumbnail(client.user.displayAvatarURL())
 			  .setColor('GREEN')
-			  .setFooter(keywords ? keywords.join(', ') : 'None')
+			  .setFooter(keywords)
 			  .addFields({
 			    name: 'Maintainers:',
 			    value: maintainers
@@ -69,8 +107,8 @@ client.on('message', async message => {
 			  msg.edit(embed)
 			});
 		} catch (err) {
-			msg.delete()
-			message.channel.send(new discord.MessageEmbed().setTitle(`An unexpected error occured:`).setDescription(err))
+	    msg.delete()
+	    message.channel.send(new discord.MessageEmbed().setTitle(`An unexpected error occured:`).setDescription(err))
 		}
 	}
 });
