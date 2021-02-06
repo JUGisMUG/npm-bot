c49877cbd432dd1db77f9a8200ec983607b66daf
JUGisMUG <70059840+JUGisMUG@users.noreply.github.com>
2021-02-06 20:24:05 +0500

    Fix bugs

--- index.js
+++ index.js
@@ -13,26 +13,13 @@ client.on('ready', () => {
 client.on('message', async message => {
 	if (message.author.bot || !message.content.startsWith(prefix)) return;
 
-	const args = message.content
-		.slice(prefix.length)
-		.trim()
-		.split(/ +/g);
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
 	const cmd = args.shift().toLowerCase();
 
 	if (cmd === 'info') {
-		if (!args[0])
-			return message.channel.send(
-				'Please provide the npm package for which you need information.'
-			);
+		if (!args[0]) return message.channel.send('Please provide the npm package for which you need information.');
 
-		let msg = await message.channel.send(
-			new discord.MessageEmbed()
-				.setAuthor(
-					`Fetching information for: ${args[0]}`,
-					client.user.displayAvatarURL()
-				)
-				.setColor('GREEN')
-		);
+		let msg = await message.channel.send(new discord.MessageEmbed().setAuthor(`Fetching information for: ${args[0]}`, client.user.displayAvatarURL()).setColor('GREEN'));
 		
 		try {
 		  fetch(`https://registry.npmjs.org/${args[0]}`)
@@ -44,47 +31,46 @@ client.on('message', async message => {
 			  let repository = response.versions[latest].repository.url || 'None'
 			  let keywords = response.versions[latest].keywords
 			  let maintainers = response.versions[latest].maintainers.map(object => object.name).join(', ')
-			  let typescript = () => {
-			    if (response.versions[latest].types) return 'Yes'
-			    if (response.versions[latest].types === undefined) return 'No'
-			  }
 			  
-			  if (keywords === undefined) {
-			    keywords = 'None'
-			  } else if (keywords !== undefined) keywords = keywords.join(', ')
+			  let dependencies;
+			  if (response.versions[latest].dependencies === undefined || null) {
+			    dependencies = 'None';
+			  } else {
+			    dependencies = Object.keys(response.versions[latest].dependencies).join(', ');
+			  }
 			  
 			  if (repository.includes('git+')) repository = repository.replace('git+', '')
 			  if (repository.includes('.git')) repository = repository.replace('.git', '')
 			  
 			  let embed = new discord.MessageEmbed()
-			  .setAuthor(args[0] + ' - ' + latest, 'https://i.gyazo.com/3b2e93eb22aff52694daf56b5cf1ad75.png')
-			  .setURL(`https://www.npmjs.com/package/${args[0]}`)
-			  .setDescription(response.description || 'None')
+			  .setAuthor(`${args[0]} - ${latest}`, 'https://i.gyazo.com/3b2e93eb22aff52694daf56b5cf1ad75.png')
+			  .setURL(`https://npmjs.com/package/${args[0]}`)
+			  .setDescription(response.description ? response.description : 'None')
 			  .setThumbnail(client.user.displayAvatarURL())
 			  .setColor('GREEN')
-			  .setFooter(keywords)
+			  .setFooter(keywords ? keywords.join(', ') : 'None')
 			  .addFields({
 			    name: 'Maintainers:',
 			    value: maintainers
 			  }, {
 			    name: 'Dependencies:',
-			    value: Object.keys(response.versions[latest].dependencies).join(', ') || 'None'
+			    value: dependencies
 			  }, {
 			    name: 'License:',
-			    value: response.versions[latest].license || 'None'
+			    value: response.versions[latest].license ? response.versions[latest].license : 'None'
 			  }, {
 			    name: 'Repository:',
 			    value: `[GitHub Repository](${repository})`
 			  }, {
-			    name: 'Typescript Supoort:',
-			    value: typescript()
+			    name: 'Typescript Support:',
+			    value: response.versions[latest].types ? 'Yes' : 'No'
 			  })
 			  
 			  msg.edit(embed)
 			});
 		} catch (err) {
-	    msg.delete()
-	    message.channel.send(new discord.MessageEmbed().setTitle(`An unexpected error occured:`).setDescription(err))
+			msg.delete()
+			message.channel.send(new discord.MessageEmbed().setTitle(`An unexpected error occured:`).setDescription(err))
 		}
 	}
 });
